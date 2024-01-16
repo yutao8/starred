3243512a1718b4e81af95eecd3a473f8c8fbc978
yutao8 <8745770+yutao8@users.noreply.github.com>
2024-01-16 14:43:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,11 @@ is_dir(CACHE_PATH) or mkdir(CACHE_PATH, 0777, true);
 define('GH_TOKEN', $_ENV['GH_TOKEN'] ?? null); //github token  github_pat_xxxxx  https://github.com/settings/tokens?type=beta
 define('GPT_URL', $_ENV['GPT_URL'] ?? null); //chatgpt api url
 define('GPT_KEY', $_ENV['GPT_KEY'] ?? null); //chatgpt key
+$username = isset($_ENV['GITHUB_ACTOR']) ? $_ENV['GITHUB_ACTOR'] : 'yutao8';
 
 GH_TOKEN or die('GH_TOKEN is null!');
 
-$listAll = getAllStarList('yutao8');
+$listAll = getAllStarList($username);
 makeMarkdown($listAll);
 echo file_get_contents('index.html');
 
